aaedce9e35721e323b4b9ea1cd7e455a66da3307
AIR <air.cse.pbn@gmail.com>
2015-02-08 10:55:46 +0600

    Day-3 documents

---
+++ Day-3/HomeWorkThree.java
@@ -0,0 +1,38 @@
+// Author: S Mahbub-Uz Zaman : 27 January
+
+public class HomeWorkThree {
+    public static void main (String args []) {
+                
+        
+    /*  Problem 1 : Write a program that prompts the user to enter 
+    two positive integers and finds their greatest common divisor.  
+    Solution:  Suppose you enter two integers 4 and 2, their greatest 
+    common divisor is 2. Suppose you enter two integers 16 and 24, 
+    their greatest common divisor is 8. So, how do you find the 
+    greatest common divisor? Let the two input integers be n1 and n2. 
+    You know number 1 is a common divisor, but it may not be the 
+    greatest commons divisor. So you can check whether 
+    k (for k = 2, 3, 4, and so on) is a common divisor for 
+    n1 and n2, until k is greater than n1 or n2.  
+    */
+        
+     /*  Problem 2 : Write a program that displays the first 50 
+     prime numbers in five lines, each of which contains 10 
+     numbers. An integer greater than 1 is prime if its only 
+     positive divisor is 1 or itself. For example, 2, 3, 5, and 7 
+     are prime numbers, but 4, 6, 8, and 9 are not. 
+     Solution: The problem can be broken into the following tasks: 
+     For number = 2, 3, 4, 5, 6, ..., test whether the number is prime. 
+     Determine whether a given number is prime. Count the prime numbers. 
+     Print each prime number, and print 10 numbers per line.  
+     */
+        
+        
+     // Deadline January 30, 2015 Night
+     // You can not use any algorithm
+     // Please do both the solution in a single file
+     // Please for more rules check the facebook group
+        
+    
+    }
+}
